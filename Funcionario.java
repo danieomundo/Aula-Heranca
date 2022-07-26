052b7f96fccc7e26092d2526dafdd257e070c147
Dani <daniele.ddsp@gmail.com>
2022-07-26 03:47:36 -0300

    funcionario

---
+++ Funcionario.java
@@ -0,0 +1,15 @@
+public class Funcionario extends Pessoa {
+
+    private String dataAdmissao;
+
+    public void promover() {
+        System.out.println("Funcionario promovido.");
+    }
+
+    public void aposentar() {
+        System.out.println("Funcionario aposentado.");
+    }
+
+
+
+}
