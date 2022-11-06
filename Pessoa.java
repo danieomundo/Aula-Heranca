f63df678bc920b0999b70c6ebb5096dcbeba982f
Dani <daniele.ddsp@gmail.com>
2022-11-06 12:53:34 -0300

    Update Pessoa.java

--- Pessoa.java
+++ Pessoa.java
@@ -5,25 +5,53 @@ public class Pessoa {
     protected String telefone;
     protected String email;
 
-    public void atualizarContato(String endereco, String telefone, String email) {
-
-        this.endereco = endereco;
-        this.telefone = telefone;
-        this.email = email;
-
-    }
-
+    // métodos getters e setters
+    
     public void setNome(String nome) {
         this.nome = nome;
     }
 
-    public void imprimePessoa() {
+    public String getNome() {
+        return this.nome;
+    }
+    
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;   
+    }
+    
+    public String getEndereco() {
+        return this.endereco;
+    }
+    
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+        
+    public String getTelefone() {
+        return this.telefone;
+    }
+    
+    public void setEmail(String email){
+        this.email = email;
+    }
 
+    public String getEmail() {
+        return this.email;
+    }
+    
+    // métodos
+           
+    public void atualizarContato(String endereco, String telefone, String email) {
+        this.endereco = endereco;
+        this.telefone = telefone;
+        this.email = email;
+    }
+    
+    public void imprimePessoa() {
         System.out.println("Nome: " + this.nome);
         System.out.println("Endereco: " + this.endereco);
         System.out.println("Telefone: " + this.telefone);
         System.out.println("E-mail: " + this.email);
-
     }
 
 }
